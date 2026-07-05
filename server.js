552c7c1ac4da170deaa327d96fc67f08a0a98581
Darshan Ahire <ahiredarshan399@gmail.com>
2026-07-05 22:49:22 +0530

    fix cors

--- server.js
+++ server.js
@@ -10,14 +10,26 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://police-management-portal-frontend.vercel.app",
-  process.env.FRONTEND_URL,
+  "https://police-management-port-git-77d0e3-vighnahartasoftwares-projects.vercel.app",
 ];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin(origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+
     credentials: true,
+
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
