54d5c1138a8a83cdc087b9a024ef0f52805f1785
Darshan Ahire <ahiredarshan399@gmail.com>
2026-07-05 23:28:34 +0530

    ready for production deploy

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-db.connect((error) => {
-  if (error) {
-    console.error("MySQL connection failed:", error.message);
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL Connection Failed:", err.message);
     return;
   }
 
